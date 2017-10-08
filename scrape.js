c8847b260081890158565033fe38348db3dcdf74
Shloak Jain <shloak@gmail.com>
2017-10-07 17:29:34 -0700

    array

--- scrape.js
+++ scrape.js
@@ -1,11 +1,11 @@
 var request = require('request'),
 	cheerio = require('cheerio'),
 	urls = [];
-var types = ["shirt", "jacket", "hat", "pants", "shoes"]
+var types = ["shirt", "jacket", "hat", "pants", "shoes"];
 var occassions = ["casual", "business", "summer", "winter", "sport"];
-var colors = ["black", "white", "grey", "gray", "red", "orange", "yellow", "green", "blue", "violet", "pink", "purple", "brown"]
-var styles = [["collar", "v-neck", "v neck", "crewneck"], [""], ["snapback", "baseball", "beanie"], ["jeans", "slacks", "shorts", "khaki", "sweatpant"], ["tennis", "flat", "skate", ""]];
-var fabrics = ["cotton", "denim", "leather", "nylon", "polyster"]
+var colors = ["black", "white", "grey", "gray", "red", "orange", "yellow", "green", "blue", "violet", "pink", "purple", "brown"];
+var styles = [["collar", "v-neck", "v neck", "crewneck"], ["flannel", "windbreaker", "sports"], ["snapback", "baseball", "beanie"], ["jeans", "slacks", "shorts", "khaki", "sweatpant"], ["tennis", "flat", "skate", ""]];
+var fabrics = ["cotton", "denim", "leather", "nylon", "polyster"];
 
 
 request('http://www.ae.com/men-aeo-plaid-oxford-shirt-white/web/s-prod/0153_9873_100?cm=sUS-cUSD&catId=cat6300043', function(err, resp, body){ //  
@@ -17,7 +17,9 @@ request('http://www.ae.com/men-aeo-plaid-oxford-shirt-white/web/s-prod/0153_9873
 });
 
 function occassion(){
-
+	for (i = 0; i < occassions.length; i++){
+		if (text.inclide)
+	}
 	text.includes()
 }
 
