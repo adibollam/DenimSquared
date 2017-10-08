20d7134d105068374642e5a63bd36a38bba32cc0
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2017-10-07 22:17:10 -0700

    cleaned up data

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,7 @@
 var types = ["shirt", "jacket", "pants", "shoes"];
 var occassions = ["casual", "business", "summer", "winter", "sport"];
 var colors = ["black", "white", "grey", "gray", "red", "orange", "yellow", "green", "blue", "violet", "pink", "purple", "brown"];
-var styles = [["collar", "v-neck", "crewneck", "t-shirt"],
+var styles = [["button", "v-neck", "fullsleeve", "t-shirt"],
 ["flannel", "windbreaker", "sports", "hoodie"],
 ["jeans", "slacks", "shorts", "khaki", "sweatpant"],
 ["tennis", "flat", "skate", ""]];
@@ -11,42 +11,47 @@ var fabrics = ["cotton", "denim", "leather", "nylon", "polyster"];
 var input = {"type": 'shirt',
             "occassion": 'casual',
             "color": 'black',
-            "style": 't-shirt'};
+            "style": 't-shirt',
+            "fabrics": 'cotton'};
 
 var shirt = blob["shirt"];
 var jacket = blob["jacket"];
 var pants = blob["pants"];
 var shoes = blob["shoes"];
 
-if (input.keys() == 'shirt'){
-  goodJackets = [];
-  for (i = 0; i < jacket.length; i++)
-  {
-    if (jacket[i]["occassion"] == input["occassion"]) goodJackets.append(jacket[i])
+function ocassion(article){
+    goodArticle = [];
+    for (i = 0; i < article.length; i++)
+    {
+      if (article[i]["occassion"] == input["occassion"]) goodArticle.append(article[i])
+    }
+
+  return goodArticle
+}
+
+function styles(article, styleIn){
+  if (input['type'] == 'shirt'){
+    goodArticle = [];
+    if (styleIn == 't-shirt'){
+      for (i = 0; i < article.length; i++){
+        if (article[i]["styles"] != 'slacks') goodArticle.append(article[i])
+      }
+    }
+    if (styleIn == 'fullsleeve'){
+      for (i = 0; i < article.length; i++){
+        if (['flannel', 'slacks'].indexOf(article[i]['styles']) == -1) goodArticle.append(article[i])
+      }
+    }
+    if (styleIn == 'v-neck'){
+      for (i = 0; i < article.length; i++){
+        if (['flannel', 'slacks', 'sports'].indexOf(article[i]['styles']) == -1) goodArticle.append(article[i])
+      }
+    }
+    if (styleIn == 'button'){
+      for (i = 0; i < article.length; i++){
+        if (['flannel', 'hoodie', 'sports', 'sweatpant', 'tennis'].indexOf(article[i]['styles']) == -1) goodArticle.append(article[i])
+      }
+    }
   }
-  
-}
-
-for (i = 0; i < hat.length; i++)
-{
-  shirt += shirt[i][input['shirt'][0]];
-}
-
-
-for (i = 0; i < hat.length; i++)
-{
-  jacket += jacket[i][input['shirt'][0]];
-}
-
-
-for (i = 0; i < hat.length; i++)
-{
-  pants += pants[i][input['shirt'][0]];
-}
-
-
-for (i = 0; i < hat.length; i++)
-{
-  shoes += jacket[i][input['shirt'][0]];
-
+  return goodArticle
 }
